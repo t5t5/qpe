33cf9fa112540e1adf4921537d89409316706785
Evgeniy Padalko <t5t5@inbox.ru>
2019-07-13 23:38:12 +0500

    QpeLogger. File appender. Header pattern added.

--- src/logger/appenders/appender_file.cpp
+++ src/logger/appenders/appender_file.cpp
@@ -9,9 +9,10 @@
 #include <QRegularExpression>
 #include <QStandardPaths>
 
-#include "../core/loggerevent.h"
-
+#include <Qpe/Core/DateStringifier>
 #include <Qpe/Core/Enum>
+
+#include "../core/loggerevent.h"
 
 namespace Qpe
 {
@@ -19,27 +20,36 @@ namespace Qpe
 namespace LoggerPrivate
 {
 
-static const QLatin1String REG_EX_FILENAME1		("^([A-Za-z]:[\\\\" "\\/])?(.*)");
-static const QLatin1String REG_EX_FILENAME2		("[*:?\"<>|]");
+static const QRegularExpression REGEXP_FILENAME_MATCH	("^([A-Za-z]:[\\\\" "\\/])?(.*)");
+static const QRegularExpression REGEXP_FILENAME_REPLACE	("[*:?\"<>|]");
 
-static const QLatin1String PROP_HEADER			("header");
-static const QLatin1String PROP_FLUSH			("immediateFlush");
-static const QLatin1String PROP_CONTROLLER_TIME	("controllerCreationTime");
-static const QLatin1String PROP_ENCODING		("encoding");
-static const QLatin1String PROP_FILE_CACHE_SIZE	("filecachesize");
-static const QLatin1String PROP_FILE_NAME		("fileName");
+static const QLatin1String PROP_HEADER					("header");
+static const QLatin1String PROP_HEADER_PATTERN			("header.pattern");
+static const QLatin1String PROP_HEADER_PATTERN_DEFAULT	("%40E{padding=-} %T %F %40E{padding=-}\n");
+static const QLatin1String PROP_FLUSH					("flush");
+static const QLatin1String PROP_CONTROLLER_TIME			("controllerCreationTime");
+static const QLatin1String PROP_ENCODING				("encoding");
+static const QLatin1String PROP_FILE_CACHE_SIZE			("fileCacheSize");
+static const QLatin1String PROP_FILE_NAME				("fileName");
 
-static const QLatin1String PROP_TYPE			("type");
-static const QLatin1String PROP_INDEX			("index");
-static const QLatin1String INDEX_LAST			("last");
+static const QLatin1String VALUE_UNDERSCORE				("_");
 
-static const QLatin1String FORMAT_TIME			("yyyy-MM-dd hh:mm:ss,zzz");
-static const QLatin1String FORMAT_HEADER		("%1 %2 - %3 %4\n");
+static const QLatin1String EN__STDPATH					("stdPath");
+static const QLatin1String EN__STDPATH__SHORT			("s");
+static const QLatin1String PROP__STDPATH__TYPE			("type");
+static const QLatin1String PROP__STDPATH__INDEX			("index");
+static const QLatin1String INDEX_LAST					("last");
 
-static const QLatin1String VALUE_UNDERSCORE		("_");
+static const QLatin1String EN__TIME						("time");
+static const QLatin1String EN__TIME__SHORT				("T");
+static const QLatin1String PROP__TIME__FORMAT			("format");
+static const QLatin1String PROP__TIME__FORMAT__DEFAULT	("yyyy-MM-dd hh:mm:ss,zzztt");
 
-static const QLatin1String EN_s					("s");
-static const QLatin1String EN_stdPath			("stdPath");
+static const QLatin1String EN__EMPTY					("empty");
+static const QLatin1String EN__EMPTY__SHORT				("E");
+
+static const QLatin1String EN__FILE						("fileName");
+static const QLatin1String EN__FILE__SHORT				("F");
 
 #define FILES_CACHE_SIZE 32
 #define FILES_HEADERS_CACHE_SIZE 1024
@@ -55,16 +65,16 @@ struct FileNameFactory
 
 QString FileNameFactory::createExpression(const QString& en, const QVariantMap& p)
 {
-	if ((en == EN_s) || (en == EN_stdPath)) {
+	if ((en == EN__STDPATH__SHORT) || (en == EN__STDPATH)) {
 		typedef Enum<QStandardPaths::StandardLocation> StdLocation;
 
-		StdLocation locationType(p.value(PROP_TYPE).toByteArray(),
+		StdLocation locationType(p.value(PROP__STDPATH__TYPE).toByteArray(),
 			QStandardPaths::HomeLocation, Qt::CaseInsensitive);
 
 		QString location;
 		QStringList locations = QStandardPaths::standardLocations(locationType.value());
 		if (!locations.isEmpty()) {
-			QVariant index = p.value(PROP_INDEX);
+			QVariant index = p.value(PROP__STDPATH__INDEX);
 			location = (index.toString() == INDEX_LAST)
 				? locations.last()
 				: locations.value(index.toInt());
@@ -76,157 +86,257 @@ QString FileNameFactory::createExpression(const QString& en, const QVariantMap& 
 
 QStringList FileNameFactory::expressionNames()
 {
-	return QStringList() << EN_s << EN_stdPath;
+	return { EN__STDPATH, EN__STDPATH__SHORT };
 }
 
-// --------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
+class FileHelper;
+typedef Qpe::ConversionPattern<FileHelper> HeaderPattern;
 
 class FileHelper
 {
 public:
-	inline FileHelper()
-		: _fileHeaders(FILES_HEADERS_CACHE_SIZE)
-		, _file(nullptr)
-		, _codec(nullptr)
-		, _headerEnabled(true)
-		, _immediateFlush(true)
-		, _filenameRegex1(REG_EX_FILENAME1)
-		, _filenameRegex2(REG_EX_FILENAME2)
-	{
-	}
-	virtual ~FileHelper() { }
+	FileHelper();
+	virtual ~FileHelper() = default;
 
-	virtual bool initialize(const QVariantMap& properties)
-	{
-		_headerEnabled = properties.value(PROP_HEADER, true).toBool();
-		_immediateFlush = properties.value(PROP_FLUSH, true).toBool();
-		if (_headerEnabled) {
-			_headerTime = properties
-				.value(PROP_CONTROLLER_TIME, QDateTime::currentDateTime())
-				.toDateTime().toString(FORMAT_TIME);
-		}
-		return initializeCodec(properties.value(PROP_ENCODING).toByteArray());
-	}
+	virtual bool initialize(const QVariantMap& properties);
+	virtual void changeFile(const LoggerEvent* loggerEvent);
 
-	virtual void changeFile(const LoggerEvent* loggerEvent)
-	{
-		Q_UNUSED(loggerEvent)
-	}
+	QString fileName() const;
+	QDateTime headerTime() const;
 
-	inline void setFile(QFile* file) { _file = file; }
-	inline bool write(const QString& text)
-	{
-		_file->write(_codec->fromUnicode(text));
-		if (_immediateFlush)
-			_file->flush();
-		return true;
-	}
-	inline bool open()
-	{
-		return
-			isFileOpen() || (
-				createPath() &&
-				openFile() &&
-				writeHeader()
-			);
-	}
+	void setFile(QFile* file);
+	bool write(const QString& text) const;
+
+	bool open();
 protected:
-	inline QString correctFileName(const QString& filename) const
-	{
-		QRegularExpressionMatch m = _filenameRegex1.match(filename);
-		Q_ASSERT(m.hasMatch());
-		return m.captured(1).append(m.captured(2).replace(_filenameRegex2, VALUE_UNDERSCORE));
-	}
+	static QString correctFileName(const QString& filename);
 private:
-	inline bool initializeCodec(const QByteArray& encoding)
-	{
-		return
-			(_codec = QTextCodec::codecForName(encoding)) ||
-			(_codec = QTextCodec::codecForName("utf-8"));
-	}
-	inline bool isFileOpen() { return _file->isOpen(); }
-	inline bool createPath()
-	{
-		QDir dir = QFileInfo(_file->fileName()).absoluteDir();
-		return QDir().mkpath(dir.path());
-	}
-	inline bool openFile() {
-		return _file->open(
-			QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text);
-	}
-	inline bool writeHeader()
-	{
-		if	(_headerEnabled &&
-			(_fileHeaders.object(_file->fileName()) == nullptr))
-		{
-			QString s(40, '-');
-			s = QString(FORMAT_HEADER)
-				.arg(s, _headerTime, _file->fileName(), s);
-			write(s);
-			_fileHeaders.insert(_file->fileName(), new int(1));
-		}
-		return true;
-	}
-	QCache<QString, int> _fileHeaders;
-	QFile* _file;
-	QTextCodec* _codec;
-	QString _headerTime;
-	bool _headerEnabled;
-	bool _immediateFlush;
-	QRegularExpression _filenameRegex1;
-	QRegularExpression _filenameRegex2;
+	bool initializeCodec(const QByteArray& encoding);
+	bool isFileOpen() const;
+	bool createPath() const;
+	bool openFile();
+	bool writeHeader();
+private:
+	QCache<QString, int> fileHeaders;
+	QFile* file;
+	QTextCodec* codec;
+	QDateTime time;
+	HeaderPattern headerPattern;
+	bool headerEnabled;
+	bool immediateFlush;
 };
+
+// --------------------------------------------------------------------
+
+struct HeaderFactory
+{
+	static HeaderPattern::ExpressionFunctor createExpression(
+		const QString& expressionName, const QVariantMap& properties);
+	static QStringList expressionNames();
+};
+HeaderPattern::ExpressionFunctor HeaderFactory::createExpression(
+	const QString& en, const QVariantMap& p)
+{
+	if ((en == EN__TIME) || (en == EN__TIME__SHORT)) {
+		Qpe::DateStringifier s;
+		s.setFormatString(
+			p.value(PROP__TIME__FORMAT,
+					PROP__TIME__FORMAT__DEFAULT).toString());
+		return [s] (const FileHelper& d) { return s.format(d.headerTime()); };
+	} else
+	if ((en == EN__FILE) || (en == EN__FILE__SHORT)) {
+		return [] (const FileHelper& d) { return d.fileName(); };
+	} else
+	if ((en == EN__EMPTY) || (en == EN__EMPTY__SHORT)) {
+		return [] (const FileHelper&) { return QString(); };
+	}
+	return [] (const FileHelper&) { return QString(); };
+}
+
+QStringList HeaderFactory::expressionNames()
+{
+	return
+	{
+		EN__TIME, EN__TIME__SHORT,
+		EN__FILE, EN__FILE__SHORT,
+		EN__EMPTY, EN__EMPTY__SHORT
+	};
+}
+
+// --------------------------------------------------------------------
+
+FileHelper::FileHelper()
+	: fileHeaders(FILES_HEADERS_CACHE_SIZE)
+	, file(nullptr)
+	, codec(nullptr)
+	, headerEnabled(true)
+	, immediateFlush(true)
+{
+}
+
+bool FileHelper::initialize(const QVariantMap& properties)
+{
+	immediateFlush = properties.value(PROP_FLUSH, true).toBool();
+
+	time = properties.value(
+		PROP_CONTROLLER_TIME, QDateTime::currentDateTime()).toDateTime();
+
+	headerEnabled = properties.value(PROP_HEADER, true).toBool();
+	if (headerEnabled) {
+		QString p = properties.value(
+			PROP_HEADER_PATTERN, PROP_HEADER_PATTERN_DEFAULT).toString();
+		headerEnabled = !p.isEmpty();
+		headerPattern.initialize(p,
+			HeaderFactory::expressionNames(), &HeaderFactory::createExpression);
+	}
+	return initializeCodec(properties.value(PROP_ENCODING).toByteArray());
+}
+
+void FileHelper::changeFile(const LoggerEvent* loggerEvent)
+{
+	Q_UNUSED(loggerEvent);
+}
+
+QString FileHelper::fileName() const
+{
+	return file->fileName();
+}
+
+QDateTime FileHelper::headerTime() const
+{
+	return time;
+}
+
+void FileHelper::setFile(QFile* f)
+{
+	file = f;
+}
+
+bool FileHelper::write(const QString& text) const
+{
+	file->write(codec->fromUnicode(text));
+	if (immediateFlush)
+		file->flush();
+	return true;
+}
+
+bool FileHelper::open()
+{
+	return isFileOpen() || (createPath() && openFile() && writeHeader());
+}
+
+QString FileHelper::correctFileName(const QString& filename)
+{
+	QRegularExpressionMatch m = REGEXP_FILENAME_MATCH.match(filename);
+	Q_ASSERT(m.hasMatch());
+	return m.captured(1)
+			.append(m.captured(2).replace(REGEXP_FILENAME_REPLACE, VALUE_UNDERSCORE));
+}
+
+bool FileHelper::initializeCodec(const QByteArray& encoding)
+{
+	return
+		(codec = QTextCodec::codecForName(encoding)) ||
+		(codec = QTextCodec::codecForName("utf-8"));
+}
+
+bool FileHelper::isFileOpen() const
+{
+	return file->isOpen();
+}
+
+bool FileHelper::createPath() const
+{
+	QDir dir = QFileInfo(file->fileName()).absoluteDir();
+	return QDir().mkpath(dir.path());
+}
+
+bool FileHelper::openFile()
+{
+	return file->open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text);
+}
+
+bool FileHelper::writeHeader()
+{
+	if  (headerEnabled &&
+		(fileHeaders.object(fileName()) == nullptr))
+	{
+		QString h = headerPattern.createString(*this);
+		write(h);
+		fileHeaders.insert(fileName(), new int(1));
+	}
+	return true;
+}
+
+// ------------------------------------------------------------------------
 
 class FileHelperSingle : public FileHelper
 {
 public:
-	FileHelperSingle(const QString& fileName)
-		: FileHelper()
-		, file(correctFileName(fileName))
-	{
-		setFile(&file);
-	}
+	FileHelperSingle(const QString& fileName);
+	virtual ~FileHelperSingle() Q_DECL_OVERRIDE = default;
 private:
 	QFile file;
 };
+
+FileHelperSingle::FileHelperSingle(const QString& fileName)
+	: FileHelper()
+	, file(correctFileName(fileName))
+{
+	setFile(&file);
+}
+
+// ------------------------------------------------------------------------
 
 class FileHelperCached : public FileHelper
 {
 	typedef QCache<QString, QFile> Files;
 public:
-	FileHelperCached(const Pattern& fileNamePattern)
-		: FileHelper()
-		, fileNamePattern(fileNamePattern)
-		, files(FILES_CACHE_SIZE)
-	{
-	}
-	virtual bool initialize(const QVariantMap& properties)
-	{
-		return
-			FileHelper::initialize(properties) &&
-			initializeCache(properties.value(PROP_FILE_CACHE_SIZE).toInt());
-	}
-	virtual void changeFile(const LoggerEvent* loggerEvent)
-	{
-		EventWrapper ew(loggerEvent);
-		QString fileName = correctFileName(fileNamePattern.createString(ew));
-		QFile* f = files.object(fileName);
-		if (f == nullptr) {
-			f = new QFile(fileName);
-			files.insert(fileName, f);
-		}
-		setFile(f);
-	}
+	FileHelperCached(const Pattern& fileNamePattern);
+	virtual ~FileHelperCached() Q_DECL_OVERRIDE = default;
+
+	virtual bool initialize(const QVariantMap& properties) Q_DECL_OVERRIDE;
+	virtual void changeFile(const LoggerEvent* loggerEvent) Q_DECL_OVERRIDE;
 private:
-	inline bool initializeCache(int fileCacheSize)
-	{
-		if (fileCacheSize > 0)
-			files.setMaxCost(fileCacheSize);
-		return true;
-	}
+	bool initializeCache(int fileCacheSize);
 	Pattern fileNamePattern;
 	Files files;
 };
+
+FileHelperCached::FileHelperCached(const Pattern& fileNamePattern)
+	: FileHelper()
+	, fileNamePattern(fileNamePattern)
+	, files(FILES_CACHE_SIZE)
+{
+}
+
+bool FileHelperCached::initialize(const QVariantMap& properties)
+{
+	return
+		FileHelper::initialize(properties) &&
+		initializeCache(properties.value(PROP_FILE_CACHE_SIZE).toInt());
+}
+
+void FileHelperCached::changeFile(const LoggerEvent* loggerEvent)
+{
+	EventWrapper ew(loggerEvent);
+	QString fileName = correctFileName(fileNamePattern.createString(ew));
+	QFile* f = files.object(fileName);
+	if (f == nullptr) {
+		f = new QFile(fileName);
+		files.insert(fileName, f);
+	}
+	setFile(f);
+}
+
+bool FileHelperCached::initializeCache(int fileCacheSize)
+{
+	if (fileCacheSize > 0)
+		files.setMaxCost(fileCacheSize);
+	return true;
+}
 
 } // namespace LoggerPrivate
 
--- src/tests/test_logger/test_logger.cpp
+++ src/tests/test_logger/test_logger.cpp
@@ -16,19 +16,20 @@ private slots:
 void TestLogger::initTestCase()
 {
 	QVariantMap s;
-	s["logger.root.appenders"]   = "root";
-	s["logger.root.filters"]     = "root";
+	s["logger.root.appenders"]         = "root";
+	s["logger.root.filters"]           = "root";
 
-	s["logger.TEST_SINGLE.appenders"] = "TEST_SINGLE";
-	s["logger.TEST_SINGLE.filters"]   = "root";
+	s["logger.TEST_SINGLE.appenders"]  = "TEST_SINGLE";
+	s["logger.TEST_SINGLE.filters"]    = "root";
 
-	s["logger.TEST_MULTI.appenders"] = "TEST_MULTI";
-	s["logger.TEST_MULTI.filters"]   = "TEST_MULTI";
+	s["logger.TEST_MULTI.appenders"]   = "TEST_MULTI";
+	s["logger.TEST_MULTI.filters"]     = "TEST_MULTI";
 
-	s["appender.root.type"]      = "FileAppender";
-	s["appender.root.fileName"]  = "log/%d{format=yyyy-MM-dd}/root_%N__%c.log.txt";
-	s["appender.root.pattern.0"] = "e:%e{format=10l} (l=%l{format=10l}) [T=%T] %d{format=yyyy-MM-dd HH:mm:ss,zzz} %-6p [%-25.25c] | %m\n";
-	s["appender.root.header"]    = "false";
+	s["appender.root.type"]            = "FileAppender";
+	s["appender.root.fileName"]        = "log/%d{format=yyyy-MM-dd}/root_%N__%c.log.txt";
+	s["appender.root.pattern.0"]       = "e:%e{format=10l} (l=%l{format=10l}) [T=%T] %d{format=yyyy-MM-dd HH:mm:ss,zzz} %-6p [%-25.25c] | %m\n";
+	s["appender.root.header"]          = "true";
+	s["appender.root.header.pattern"]  = "%40E{padding=-} %T %F %40E{padding=-}\n";
 
 	s["appender.TEST_SINGLE.type"]     = "FileAppender";
 	s["appender.TEST_SINGLE.fileName"] = "log/%d{format=yyyy-MM-dd}/%N__%c.log.txt";
@@ -39,8 +40,8 @@ void TestLogger::initTestCase()
 	s["appender.TEST_MULTI.pattern.0"] = "e:%e{format=10l} (l=%l{format=10l}) [T=%T] %d{format=yyyy-MM-dd HH:mm:ss,zzz} %-6p [%-25.25c] | %m\n";
 	s["appender.TEST_MULTI.pattern.1"] = "e:%-108e{format=10l}| %m\n";
 
-	s["filter.root.type"]        = "EventTypeFilter";
-	s["filter.root.eventTypes"]  = "all";
+	s["filter.root.type"]              = "EventTypeFilter";
+	s["filter.root.eventTypes"]        = "all";
 
 	s["filter.TEST_MULTI.type"]        = "EventTypeFilter";
 	s["filter.TEST_MULTI.eventTypes"]  = "info, warn";
